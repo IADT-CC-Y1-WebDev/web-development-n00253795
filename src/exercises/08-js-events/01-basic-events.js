26539309006373de840bf2028fee5c8c34fc32c9
Egshiglen Naran Ulzii <n00253795@iadt.ie>
2026-03-23 12:54:31 +0000

    games filter

--- src/exercises/08-js-events/01-basic-events.js
+++ src/exercises/08-js-events/01-basic-events.js
@@ -3,4 +3,32 @@ let toggleBoxBtn = document.getElementById('toggle_box_btn');
 let preview = document.getElementById ('preview');
 let previewInput = document.getElementById ('preview_input');
 
-console .log(box);
+toggleBoxBtn.addEventListener("click",(event)=>{
+    toggleBoxVisibility(box);
+});
+
+function toggleBoxVisibility(box) {
+    //console.log(event.target);
+    box.classList.toggle('hidden');
+
+}
+
+previewInput .addEventListener('',(event) =>{
+    updatePreview(preview, event.target.value);
+});
+
+function updatePreview(previewElement, text) {
+   //console.log(previewElement,text);
+   const trimmed = text.trim();
+
+   if(trimmed === '') {
+    previewElement.textContent = '(nothing yet)';
+    previewElement.classList.add('empty');
+   } else {
+    previewElement.textContent = trimmed;
+    previewElement.classList.remove('empty');
+   }
+
+   
+
+};
---
+++ src/exercises/08-js-events/02-games-filters.js
@@ -0,0 +1,58 @@
+let applyBtn = document.getElementById('apply_filters');
+let clearBtn = document.getElementById('clear_filters');
+
+let cards = document.querySelector('filters')
+
+let form = document.getElementById('filters')
+
+applyBtn.addEventListener('click',(event) => {
+    event.preventDefault();
+    applyFilters();
+});
+
+clearBtn.addEventListener('click',(event) => {
+    event.preventDefault();
+    clearFilters();
+});
+
+function applyFilters() {
+    // console.log("Apply filters");
+    let filters = getFilters();
+    let matches = [];
+    for(let i = 0; i != cards.length; i++) {
+        let card = cards[i];
+        matches[i] = cardMatches(card, filters);
+    }
+    console.log(matches);
+
+}
+
+function  cardMatches(crd, fltrs) {
+    // console.log(crd.dataset.title, fltrs.titleFilter);
+    let title = crd.dataset.title.toLowerCase()
+    return title.includes(fltrs.titleFilter);
+}
+
+function getFilters() {
+    const titleE1 = form.elements['title_filter'];
+    const genreE1 = form.elements['genre_filter'];
+    const platformE1 = form.elements['platform_filter'];
+    const sortE1 = form.elements['sort_by'];
+
+    let titleFilter = (titleE1.value || '').trim().toLowerCase();
+    let genreFilter = genreE1.value || '';
+    let platformFilter = platformE1.value || '';
+    let sortBy = sortE1.value  || 'title_asc';
+
+    return {
+        "titleFilter" : titleFilter,
+        "genreFilter" : genreFilter,
+        "platformFilter" : platformFilter,
+        "sortBy" : sortBy
+    };
+}
+
+function clearFilters() {
+    console.log("Clearing filters");
+}
+
