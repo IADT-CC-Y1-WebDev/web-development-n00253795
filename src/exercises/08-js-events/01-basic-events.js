aab1d96fbd2d9e65cf822181e22ca7f845ebe8b8
Egshiglen Naran Ulzii <n00253795@iadt.ie>
2026-03-23 10:36:12 +0000

    java script 08 update

---
+++ src/exercises/08-js-events/01-basic-events.js
@@ -0,0 +1,6 @@
+let box = document.getElementById('box');
+let toggleBoxBtn = document.getElementById('toggle_box_btn');
+let preview = document.getElementById ('preview');
+let previewInput = document.getElementById ('preview_input');
+
+console .log(box);
